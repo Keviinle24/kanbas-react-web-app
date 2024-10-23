00ae18164bf5e475e7e284aff8b61a97d38a07cd
Keviinle24 <156150226+Keviinle24@users.noreply.github.com>
2024-10-22 23:00:11 -0400

    Delete labs directory

--- labs/lab2.tsx
+++
@@ -1,7 +0,0 @@
-export default function lab2() {
-    return (
-        <div>
-            <h2>Lab 2</h2>
-        </div>
-    )
-}
--- labs/lab3.tsx
+++
@@ -1,7 +0,0 @@
-export default function lab3() {
-    return (
-        <div>
-            <h2>Lab 3</h2>
-        </div>
-    )
-}

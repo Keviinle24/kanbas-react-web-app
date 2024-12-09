34ff2547b9f6c3ce9b8c681173e2847f86114bb8
Keviinle24 <156150226+Keviinle24@users.noreply.github.com>
2024-12-08 22:05:57 -0500

    a6

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -47,7 +47,7 @@ export default function TOC() {
         </a>
       </li>
       <li className="nav-item">
-        <a id="wd-k" href="https://github.com/Keviinle24/kanbas-react-web-app" className="nav-link">
+        <a id="wd-k" href="https://github.com/Keviinle24" className="nav-link">
           My GitHub
         </a>
         
